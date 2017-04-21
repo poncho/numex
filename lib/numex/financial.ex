75c809a795f4095cda46d2b2bd44b76b4d39b337
Poncho Martinez <poncho.811@gmail.com>
2017-04-21 12:31:37 -0500

    IRR Funcion Added

--- lib/numex/financial.ex
+++ lib/numex/financial.ex
@@ -10,6 +10,8 @@ defmodule Numex.Financial do
     **type**: When payments are due [begin: 1, end: 0]<br />
   """
 
+  import Timex, only: [parse: 2, diff: 3]
+
   @when_type [begin: 1, end: 0]
 
   @doc """
@@ -67,6 +69,26 @@ defmodule Numex.Financial do
     exp2 = (1 + rate * @when_type[type]) / rate
 
     -(fv + pv * exp1) / (exp2 * (exp1 - 1))
+  end
+
+  @doc """
+    Return the Internal Rate of Return (IRR)
+  """
+  @spec irr([float], float, float, float) :: float
+  def irr(values, guess \\ 0.2, delta \\ 0.01, range \\ 0.001) do
+    investment = abs(values |> Enum.at(0))
+    npv_value = npv(guess, values)
+    case npv_value do
+      _ when abs(npv_value) < (investment * range) ->
+          result = (guess * 100)
+          {fixed_result, _} =
+            result
+            |> :erlang.float_to_binary(decimals: 2)
+            |> Float.parse
+          fixed_result
+      _ when npv_value > 0 -> irr(values, guess + delta)
+      _ when npv_value < 0 -> irr(values, guess - delta)
+    end
   end
 
 end
